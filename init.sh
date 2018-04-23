af906dfb96868de98a1c068b2f8d32726d7bf104
Aurelien Arino <aurelien.arino@gmail.com>
2018-04-22 17:00:48 -0700

    fix(init): added some colors and changed initial commit

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 name=""
 while [ -z "$name" ]; do
-printf 'Enter the project name: \n'
+    printf '\e[1;32m%s\e[m \e[1;37m%s\e[m\n' "?" "Enter the project name: "
     read name
 done
 
-vue create $name && yarn && yarn add i18n @drartemi/mc.js reflectos-sdk -D;
+vue create $name && cd $name && yarn && yarn add i18n @drartemi/mc.js reflectos-sdk -D;
+git init && git add --all && git commit --amend -m "chore: initial commit from @UReflect" --author="uReflect <contact@ureflect.io>";
