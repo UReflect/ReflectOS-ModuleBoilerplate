f6d9ff43dd62d7fa6a9bb51895cef5849caa4ab3
Aurelien ARINO <aurelien.arino@gmail.com>
2018-09-08 16:37:46 +0200

    fix(init): removed auto init git

--- init.sh
+++ init.sh
@@ -6,9 +6,4 @@ done
 
 vue create $name && cd $name && yarn && yarn add vue-i18n @drartemi/mc.js reflectos-sdk -D
 vue add electron-builder
-command -v git >/dev/null 2>&1 || {
-    echo "Error: git is not installed"
-    exit 1
-  }
-git init && git add --all && git commit --amend -m "chore: initial commit from @UReflect" --author="uReflect <dev@ureflect.io>";
 
